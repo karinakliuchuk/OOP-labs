78bb56ab513ffb7552e41c8986c92725f030619b
karinakliuchuk <karinakliuchuk20@gmail.com>
2025-11-29 21:57:26 +0200

    Lab1: initial version

---
+++ oop1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("=== Магічний клуб дуелянтів ===\n");
+
+        // Ініціалізація заклинань
+        var spellStun1 = new Spell("Ступефай", 15, SpellEffect.Stunning);
+        var spellStun2 = new Spell("Петрифікус Тоталус", 20, SpellEffect.Stunning);
+        var spellDisarm = new Spell("Експеліармус", 25, SpellEffect.Disarming);
+        var spellCut = new Spell("Сектумсемпра", 30, SpellEffect.Damage);
+        var spellShield = new Spell("Протеґо", 0, SpellEffect.Disarming);
+        var spellFire = new Spell("Інсендіо", 18, SpellEffect.Damage);
+        var spellLevitate = new Spell("Левікорпус", 22, SpellEffect.Stunning);
+
+        // Створення персонажів
+        var wizDumbledore = new Wizard("Альбус Дамблдор", "Гріффіндор");
+        var wizNarcissa = new Wizard("Нарциса Мелфой", "Слизерин");
+        var wizSirius = new Wizard("Сіріус Блек", "Гріффіндор");
+
+        // Навчання чарівників
+        wizDumbledore.LearnSpell(spellDisarm);
+        wizDumbledore.LearnSpell(spellShield);
+        wizDumbledore.LearnSpell(spellStun2);
+        wizDumbledore.LearnSpell(spellFire);
+
+        wizNarcissa.LearnSpell(spellCut);
+        wizNarcissa.LearnSpell(spellStun1);
+        wizNarcissa.LearnSpell(spellLevitate);
+
+        wizSirius.LearnSpell(spellStun1);
+        wizSirius.LearnSpell(spellDisarm);
+        wizSirius.LearnSpell(spellShield);
+        wizSirius.LearnSpell(spellFire);
+
+        // Створення клубу дуелей
+        var duelClub = new DuelingClub();
+
+        // Проведення боїв
+        Console.WriteLine("=== ДУЕЛЬ 1: Дамблдор проти Нарциси ===");
+        duelClub.HostDuel(wizDumbledore, wizNarcissa);
+
+        Console.WriteLine("=== ДУЕЛЬ 2: Сіріус проти Нарциси ===");
+        duelClub.HostDuel(wizSirius, wizNarcissa);
+
+        Console.WriteLine("=== ДУЕЛЬ 3: Дамблдор проти Сіріуса ===");
+        duelClub.HostDuel(wizDumbledore, wizSirius);
+
+        // Історія дуелей
+        Console.WriteLine("\n=== ЗВІТ ПРО ДУЕЛІ ===");
+        wizDumbledore.GetDuelHistory();
+        wizNarcissa.GetDuelHistory();
+        wizSirius.GetDuelHistory();
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу, щоб завершити...");
+        Console.ReadKey();
+    }
+}
---
+++ oop1/Wizard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Wizard
+{
+    public string Name { get; set; }
+    public string House { get; set; }
+    public int Health { get; set; }
+    public List<Spell> KnownSpells { get; private set; }
+    private List<DuelResult> duelHistory;
+
+    public Wizard(string name, string house)
+    {
+        Name = name;
+        House = house;
+        Health = 100;
+        KnownSpells = new List<Spell>();
+        duelHistory = new List<DuelResult>();
+    }
+
+    public void LearnSpell(Spell spell)
+    {
+        KnownSpells.Add(spell);
+    }
+
+    public Spell CastRandomSpell()
+    {
+        if (KnownSpells.Count == 0)
+        {
+            return new Spell("Силовий імпульс", 10, SpellEffect.Disarming);
+        }
+
+        Random rand = new Random();
+        return KnownSpells[rand.Next(KnownSpells.Count)];
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health < 0) Health = 0;
+    }
+
+    public void AddDuelToHistory(DuelResult duelRecord)
+    {
+        duelHistory.Add(duelRecord);
+    }
+
+    public void GetDuelHistory()
+    {
+        Console.WriteLine($"Історія дуелей чарівника {Name.ToUpper()} ({House}):");
+        Console.WriteLine($"Всього дуелей: {duelHistory.Count}");
+
+        int wins = 0;
+        int losses = 0;
+
+        foreach (var duel in duelHistory)
+        {
+            if (duel.Winner.Name == Name)
+            {
+                wins++;
+                Console.WriteLine($"Перемога #{duel.DuelId} проти {duel.Loser.Name}");
+            }
+            else
+            {
+                losses++;
+                Console.WriteLine($"Поразка #{duel.DuelId} від {duel.Winner.Name}");
+            }
+        }
+
+        if (duelHistory.Count > 0)
+        {
+            Console.WriteLine($"Статистика: {wins} перемог, {losses} поразок");
+        }
+        else
+        {
+            Console.WriteLine("Ще не брав участі в дуелях");
+        }
+        Console.WriteLine();
+    }
+}
